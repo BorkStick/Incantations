93fc45947410e27a114b2ea7a765125cbdb350b5
Unknown <4ciiDicBa55@gmail.com>
2018-03-18 12:05:58 -0500

    tweaked initial setup script

--- setup.sh
+++ setup.sh
@@ -1,22 +1,72 @@
-#!/bin/sh
+!#/bin/bash
 
-## update and upgrade
+#install script for a fresh ubuntu install
+
+#things to install
+
+#chrome
+#atom.io
+#gparted
+
+#----------
+# add repos
+#----------
+#obs
+sudo add-apt-repository ppa:obsproject/obs-studio
+
+#ATOM.IO
+sudo add-apt-repository ppa:webupd8team/atom
+
+
+
+#----------
+# basic update
+#----------
+#check for upgrades then check for upgrades
 sudo apt update -y && sudo apt upgrade -y
 
-## install all the apps
-AppList="vim htop tmux  dconf-cli git"
+#----------
+# install apps
+#----------
+
+# install all the apps
+AppList="vim htop tmux ffmpeg gdebi build-essential ffmpeg gdebi build-essential filezilla git curl gparted cmatrix exfat-fuse exfat-utils dconf-cli software-properties-common openssh-server atom ranger obs-studio "
 sudo apt install -y $AppList
 
-# deb installs
-## install gitkraken
+
+#GITKRACKEN
 wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
 sudo dpkg -i gitkraken-amd64.deb
 
 
-# Things that need user input
+#----------
+# scripts
+#----------
 
-# install atom
-## add repo
-sudo add-apt-repository ppa:webupd8team/atom
-## update and install atom
-sudo apt update -y; sudo apt install atom -y
+#----------
+# dotfiles
+#----------
+#tmux
+git clone https://github.com/4ciiDic/tmux.conf.git ~/.tmux
+ln -s ~/.tmux/.tmux.conf ~/.tmux.conf
+
+#kodi
+
+#----------
+# update system settings
+#----------
+
+#----------
+# requires clicks
+#----------
+sudo apt-get install -y ubuntu-restricted-extras
+
+#----------
+# prompt for a reboot
+#----------
+clear
+echo ""
+echo "===================="
+echo " TIME FOR A REBOOT! "
+echo "===================="
+echo ""
