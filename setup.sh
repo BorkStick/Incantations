573f259a6e4967371edd64115ad069b86023bd23
Unknown <4ciiDicBa55@gmail.com>
2018-03-19 06:57:46 -0500

    Added to the init script

--- setup.sh
+++ setup.sh
@@ -17,6 +17,10 @@ sudo add-apt-repository ppa:obsproject/obs-studio
 #ATOM.IO
 sudo add-apt-repository ppa:webupd8team/atom
 
+#chrome repository
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+
 
 
 #----------
@@ -30,7 +34,7 @@ sudo apt update -y && sudo apt upgrade -y
 #----------
 
 # install all the apps
-AppList="vim htop tmux ffmpeg gdebi build-essential ffmpeg gdebi build-essential filezilla git curl gparted cmatrix exfat-fuse exfat-utils dconf-cli software-properties-common openssh-server atom ranger obs-studio "
+AppList="vim i3 rofi htop tmux ffmpeg gdebi build-essential ffmpeg filezilla git curl gparted cmatrix exfat-fuse exfat-utils dconf-cli software-properties-common openssh-server atom ranger obs-studio google-chrome-stable"
 sudo apt install -y $AppList
 
 
@@ -39,9 +43,15 @@ wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
 sudo dpkg -i gitkraken-amd64.deb
 
 
+
+
 #----------
 # scripts
 #----------
+#dracula theme for gnome terminal
+git clone https://github.com/GalaticStryder/gnome-terminal-colors-dracula
+./gnome-terminal-colors-dracula/install.sh
+
 
 #----------
 # dotfiles
