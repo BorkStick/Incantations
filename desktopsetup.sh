53fd84f94e3ee40ec27005130dcea781d41e410c
BorkStick <4ciiDicBa55@gmail.com>
2018-05-27 03:21:48 -0500

    updated the desktop setup script

--- desktopsetup.sh
+++ desktopsetup.sh
@@ -7,12 +7,15 @@
 #chrome
 #atom.io
 #gparted
-
+clear
 echo ""
 echo "=================================================================="
 echo " Welcome to the BorkSlash INITIAL SETUP SCRIPT FOR DESKTOP LINUX! "
 echo "=================================================================="
 echo ""
+
+#tewaked for ubuntu 18.04
+
 
 
 echo ""
@@ -38,7 +41,11 @@ echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sud
 echo "deb http://repo.sinew.in/ stable main" | sudo tee /etc/apt/sources.list.d/enpass.list
 wget -O - https://dl.sinew.in/keys/enpass-linux.key | sudo apt-key add -
 
-clear
+
+
+
+
+#clear
 echo ""
 echo "============================================="
 echo " UPDATTTTEEEEEEEEEE UPGRADDDDDDDDDEEEEEEEEE! "
@@ -50,26 +57,49 @@ echo ""
 sudo apt update -y && sudo apt upgrade -y
 
 
-clear
+
+
+
+#clear
 echo ""
 echo "========================="
 echo " INSATALL THEM PROGRAMS! "
 echo "========================="
 echo ""
 
-
+# look at this fancy shit. programing fuck yeah
 # install all the apps
-AppList="vim i3 rofi htop tmux ffmpeg gdebi build-essential ffmpeg filezilla git curl gparted cmatrix exfat-fuse exfat-utils dconf-cli software-properties-common openssh-server atom ranger obs-studio google-chrome-stable enpass vlc"
+AppList="autokey-gtk vim i3 rofi htop tmux ffmpeg gdebi build-essential ffmpeg filezilla git curl hexchat gparted cmatrix exfat-fuse exfat-utils dconf-cli software-properties-common openssh-server atom ranger obs-studio google-chrome-stable enpass vlc virtualbox gnome-tweaks vagrant"
 sudo apt install -y $AppList
 
+# install atom addons
+AtomList=" atom-beautify file-icons pigments highlight-selected linter linter-markdown markdown-writer markdown-scroll-sync platformio-ide-terminal"
+apm install -y $AtomList
 
+
+
+# todo need to make this not download to the scrips folder
+# wget to /tmp then dpkg it from there
 #GITKRACKEN
-wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
-sudo dpkg -i gitkraken-amd64.deb
+wget -P /tmp https://release.gitkraken.com/linux/gitkraken-amd64.deb
+sudo dpkg -i /tmp/gitkraken-amd64.deb
+
+# install youtube-dl
+sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+sudo chmod a+rx /usr/local/bin/youtube-dl
+
+# coool snap package
+# install discord
+sudo snap install discord
 
 
 
-clear
+# todo
+# install gnome extentions
+
+
+
+#clear
 echo ""
 echo "===================="
 echo " SCRIPT TIME! "
@@ -77,12 +107,16 @@ echo "===================="
 echo ""
 
 
+
 #dracula theme for gnome terminal
 git clone https://github.com/GalaticStryder/gnome-terminal-colors-dracula
 ./gnome-terminal-colors-dracula/install.sh
 
 
-clear
+
+
+
+#clear
 echo ""
 echo "========================="
 echo " DEEZ DEM DOTFILES! "
@@ -92,9 +126,30 @@ echo ""
 #----------
 # dotfiles
 #----------
+
+# I should be able to set this shit up so it pulls my dotfiles from github/gitlab
+# todo do get the dotfiles repo setup and working
+
 #tmux
 git clone https://github.com/4ciiDic/tmux.conf.git ~/.tmux
 ln -s ~/.tmux/.tmux.conf ~/.tmux.conf
+
+# todo atom dotfiles
+# ~/.atom
+
+# todo
+# set theme to adwaita-dark
+
+# set desktop wallpaper
+mkdir ~/Pictures/Wallpapers
+wget -P ~/Pictures/Wallpapers https://release.gitkraken.com/linux/gitkraken-amd64.deb
+gsettings set org.gnome.desktop.background picture-uri file:////home/aciidic/Pictures/Wallpapers/wallpaper.jpg
+
+
+# todo figure out how to add in my hotkeys
+# hotkeys
+
+# todo copy over autokeys
 
 #kodi
 
@@ -108,7 +163,10 @@ ln -s ~/.tmux/.tmux.conf ~/.tmux.conf
 sudo apt-get install -y ubuntu-restricted-extras
 
 
-clear
+
+
+
+#clear
 echo ""
 echo "===================="
 echo " Jobs Finished! "
