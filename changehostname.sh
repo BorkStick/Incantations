1f53f480911c4e79e18f7f31cba399552b0f140e
BorkStick <4ciiDicBa55@gmail.com>
2018-11-30 01:17:56 -0600

    got thehostname script working

--- changehostname.sh
+++ changehostname.sh
@@ -3,11 +3,23 @@
 # channge the hostname for ubuntu 18.04
 #
 
-# enter hostname
 
 
-# change hostname
+#Assign existing hostname to $hostn
+hostn=$(cat /etc/hostname)
 
-# change hosts
+#Display existing hostname
+echo "The current hostname be $hostn"
+
+#Ask for new hostname $newhost
+echo "What you want the new hostname to be: "
+read newhost
+
+#change hostname in /etc/hosts & /etc/hostname
+sudo sed -i "s/$hostn/$newhost/g" /etc/hosts
+sudo sed -i "s/$hostn/$newhost/g" /etc/hostname
+
+#display new hostname
+echo "Your new hostname is $newhost"
 
 # change cloudconfig
--- draculathemekonsole.sh
+++ draculathemekonsole.sh
@@ -2,10 +2,13 @@
 # set the konsole to the dracula theme
 #
 
-# install zip
+# download the theme file
+wget -P /tmp https://raw.githubusercontent.com/dracula/konsole/master/Dracula.colorscheme
 
 # move theme to folder
+mv /tmp/Dracula.colorscheme ~/.local/share/konsole
 
+# TODO: need to figure out how to set the theme in the profile from the cli
 # set dracula as main theme
 
 # theme will start next time konsole runs
