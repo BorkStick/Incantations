68ea794338f0521cfb0d87a0499b5a65dd12e345
Tylor Wurz <4ciiDicBa55@gmail.com>
2021-07-09 01:55:42 -0500

    setup trello board and added link in readme

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Incantations
 Here be me collection of wizzardly scripts and handy bash stuff
 
+## Kanban
+[Trello Board](https://trello.com/b/Mx9DEE9h/incantations)
+
---
+++ gnome-term-dracula.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# install the dracula theme for the gnome termanal
+
+sudo apt-get install dconf-cli
+
+
+
+git clone https://github.com/dracula/gnome-terminal
+cd gnome-terminal
+./install.sh
