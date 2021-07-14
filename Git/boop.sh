4bec9a67e858d37cec74362b82dca6de6c277a15
Tylor Wurz <4ciiDicBa55@gmail.com>
2021-07-14 01:08:15 -0500

    t

--- Git/boop.sh
+++ Git/boop.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 # script to add commit and push git repo
-COMMIT_MSG=$1
 
+# need to find a way to use string instead of arg
+# mabey ask for message and use that
+read -p "Commit message here" -n 1 -r
+echo
+
+COMMIT_MSG=$REPLY
+
+#check if message is blank
+if [[ $REPLY =~ ^\s*$ ]]; then
+echo -e "\nNo commit message" $1
+
+
+else
 
 git add .
 git commit -m "$COMMIT_MSG"
 git push
+
+fi
+
